353d505e02cadf352b6789a25d5fd5e5e2ae84d4
Luke Parker <moongateuk@gmail.com>
2018-04-21 10:16:50 +0100

    Started working on Turnbased logic and gameplay things.

---
+++ LD41/HMWTWC/Assets/Scripts/Managers/GameplayManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Managers
+{
+    public static class GameplayManager
+    {
+        public static float DeltaTime
+        {
+            get { return Time.deltaTime; }
+        }
+
+        public static bool InGame
+        {
+            get { return _inGame; }
+        }
+
+        private static bool _inGame;
+
+        public static void UpdateInGame(bool inGame)
+        {
+            _inGame = inGame;
+        }
+
+        // TODO: Add more gameplay related parts here.
+        public static List<Player> GeneratePlayers(int amount)
+        {
+            // TODO: Generate the players themselves.
+
+            // TODO: Setup their initial targets.
+
+            return new List<Player>();
+        }
+    }
+}
---
+++ LD41/HMWTWC/Assets/Scripts/Managers/TurnbasedManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Managers;
+using UnityEngine;
+
+public class TurnbasedManager : MonoBehaviour
+{
+    // Players
+    private List<Player> _playersInMatch;
+
+    // Turn countdown.
+    private float _maxTime = 5.0f;
+
+    private float _currentTime = 0.0f;
+
+    private bool _inPlacementSelection = false;
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (_inPlacementSelection && GameplayManager.InGame)
+	        TimeCheck();
+	}
+
+    public void StartGame(List<Player> players)
+    {
+        GameplayManager.UpdateInGame(true);
+    }
+
+    private void EndGame()
+    {
+        GameplayManager.UpdateInGame(false);
+    }
+
+    private void TimeCheck()
+    {
+        if (_currentTime >= _maxTime)
+        {
+            _inPlacementSelection = false;
+            StartCoroutine(TakeTurn());
+            _currentTime = 0.0f;
+        }
+        else
+        {
+            _currentTime = _currentTime + GameplayManager.DeltaTime;
+        }
+    }
+
+    private IEnumerator TakeTurn()
+    {
+        // TODO: Loop through all the players and make their moves.
+
+        // TODO: Use a Linq statement to remove those from the game.
+        // TODO: loop through to remove the gameobjects etc from the gameview.
+
+        // TODO: Recalculate the targets for all of those in the game.
+
+        // Done
+        yield return null;
+    }
+
+}
