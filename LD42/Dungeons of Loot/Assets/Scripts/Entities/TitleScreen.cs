f8fca28f3bde39a20a990268748881b8f3b87dfe
Luke Parker <moongateuk@gmail.com>
2018-08-11 19:58:21 +0100

    Added title screen, added finish scene.

---
+++ LD42/Dungeons of Loot/Assets/Scripts/Entities/TitleScreen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Rewired;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : ManagedObjectBehaviour
+{
+    private bool _sceneLoading = false;
+    private AsyncOperation _scene;
+    private Player _player;
+
+    public override void StartMe(GameObject managers)
+    {
+        _player = ReInput.players.GetPlayer(0);
+    }
+
+    public override void UpdateMe()
+    {
+        if (!_sceneLoading && _player.GetButtonDown("Select"))
+        {
+            StartCoroutine(LoadGame());
+        }
+        else if (_player.GetButtonDown("Back"))
+        {
+            Application.Quit();
+        }
+    }
+
+    IEnumerator LoadGame()
+    {
+        _scene = SceneManager.LoadSceneAsync(1);
+
+        while (!_scene.isDone)
+        {
+            yield return null;
+        }
+    }
+}
