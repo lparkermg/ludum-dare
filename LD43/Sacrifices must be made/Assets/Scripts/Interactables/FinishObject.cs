bf1ddae2122db89cf02fde4444a0fb35b4b75f17
Luke Parker <moongateuk@gmail.com>
2018-12-01 14:43:23 +0000

    Added interactables.

---
+++ LD43/Sacrifices must be made/Assets/Scripts/Interactables/FinishObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Entities;
+using Managers;
+using UnityEngine;
+
+namespace Interactables
+{
+    public class FinishObject : ManagedObjectBehaviour
+    {
+        private GameplayManagers _gameplayManager;
+
+        public override void StartMe(GameObject managers)
+        {
+            _gameplayManager = managers.GetComponent<GameplayManagers>();
+        }
+
+        public override void UpdateMe(){}
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _gameplayManager.CompleteLevel();
+            }
+        }
+    }
+}
