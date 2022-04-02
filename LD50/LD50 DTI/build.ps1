8d9444ae33a7d0223c3911bdbc8257f5f7473672
Luke Parker <moongateuk@gmail.com>
2022-04-02 11:57:52 +0100

    Initial Project Setup

---
+++ LD50/LD50 DTI/build.ps1
@@ -0,0 +1,62 @@
+param(
+    [string]$version = "0.0.0",
+    [string]$buildTarget = "Windows",
+    [string]$deployTag = "",
+    [boolean]$deploy = $False,
+    [string]$deployChannel = "windows"
+)
+
+# PROJECT SPECIFIC STUFF
+
+[string]$projectName = "Protect Thy Cargo"
+[string]$itchioProjectName = "protect-thy-cargo"
+[string]$companyName = "Luke Parker"
+[string]$itchUsername = "lparkermg"
+
+# PATHS ETC DO NOT CHANGE
+[string]$unityVersion="2019.3.10f1"
+[string]$unityPath="E:\Programs\Unity\$unityVersion\Editor\Unity.exe"
+[string]$buildLocation="$PSScriptRoot\Build\"
+
+# Functions
+Function Build([string]$target)
+{
+    Write-Host "Building $projectName ($target)"
+    Write-Host "Version: $version"
+    Write-Host ""
+    Write-Host "Generating BuildManifest File"
+
+    [string[]]$manifest = "ProductName=$projectName",
+     "CompanyName=$companyName",
+     "Version=$version",
+     "BuildLocation=$buildLocation"
+
+    Set-Content -Path "$PSScriptRoot\buildManifest.txt" -Value $manifest -Force
+    Write-Host "Build Manifest Generated"
+
+    Write-Host "Build Started"
+    $unityParams = @("-quit", "-batchMode", "-projectPath",".","-logFile", "build.log", "-executeMethod", "BuildHelper.$Target")
+    $buildProcess = Start-Process -File $unityPath -ArgumentList $unityParams -PassThru
+    Wait-Process -InputObject $buildProcess
+    Write-Host "Build Complete"
+
+    Remove-Item -Path "$PSScriptRoot\buildManifest.txt" -Force
+}
+
+Function Deploy([string]$target){
+    Write-Host "Deploying $projectName ($target)"
+    Write-Host "Version: $version"
+    Write-Host ""
+    Write-Host "Deployment Starting"
+
+    $butlerParams = @("push", "--userversion=$version", ".\Build", "$itchUsername/$itchioProjectName`:$deployChannel-$deployTag" )
+    Write-Host $butlerParams
+    butler $butlerParams
+    Write-Host "Deployment Complete"
+}
+
+Build($buildTarget)
+
+if ($deploy -eq $True){
+    Deploy($buildTarget)
+}
