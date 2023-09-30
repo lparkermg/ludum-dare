25164feaf4c1972c856a32daca4854fc05a03243
Luke Parker <lparkermg@outlook.com>
2023-09-30 13:36:35 +0100

    Starts implementing main game systems

---
+++ LD54/src/scripts/game/enums/move_enums.gd
@@ -0,0 +1,9 @@
+extends Node
+class_name MoveEnums
+
+enum Tile {
+	Up = 0,
+	Right = 1,
+	Down = 2,
+	Left = 3
+}
---
+++ LD54/src/scripts/game/main/deity/deity_system.gd
@@ -0,0 +1,34 @@
+extends Node
+class_name DeitySystem
+
+@export var ui_controller: Node
+@export var map_controller: Node
+@export var islander_system: Node
+
+signal place_at_selected_tile() # Attempts to place at selected tile (map_controller)
+signal update_ui() # Updates the UI with the request (ui_controller)
+signal select_new_tile(direction: MoveEnums.Tile) #Attempts to select a tile (map_controller)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Listen for
+		# Ui Updates
+		# Map updates
+		# Islander Updates
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	_handle_input()
+
+func _handle_input():
+	# Handle select tile
+	if Input.is_action_just_pressed("move_up"):
+		select_new_tile.emit(MoveEnums.Tile.Up)
+	elif Input.is_action_just_pressed("move_right"):
+		select_new_tile.emit(MoveEnums.Tile.Right)
+	elif Input.is_action_just_pressed("move_down"):
+		select_new_tile.emit(MoveEnums.Tile.Down)
+	elif Input.is_action_just_pressed("move_left"):
+		select_new_tile.emit(MoveEnums.Tile.Left)
+	# Note: Placing stuff is handled when UI icons are clicked.
